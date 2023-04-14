69f0282601c55a7e5eeaebf5f646b8acd03ede2e
laxmipriyapattanayak <cma.cma89@gmail.com>
2023-04-14 16:39:52 +0200

    connecting database

---
+++ server/src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const dev = require(".");
+const connectDatabase = async () => {
+     try{
+        await mongoose.connect(dev.db.url);
+        console.log("database is connected");
+     }catch (error) {
+        console.log("database is not connected");
+        console.log(error);
+     }
+};
+
+module.exports = connectDatabase;
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const dev = require("./config");
+const connectDatabase = require("./config/db");
 
  const app = express();
 
  const PORT = dev.app.serverPort;
- app.listen( PORT,() => {
+
+ app.use(morgan("dev"));
+
+ app.get( "/",(req,res) => {
+    res.status(200).send("api is running");
+ });
+
+ app.listen( PORT, async() => {
     console.log(`sserver is running at http://localhost:${PORT}`);
+    await connectDatabase();
  })
  
